abe9f5d40c4de2cca311510ac7da9f2c8baa1d0f
Weuller Krysthian R Costa <weuller.krysthian@gmail.com>
2024-03-24 13:00:15 -0300

    Adicionando autenticação via jwt

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ CREATE DATABASE `frederighi_todo_api`;
 ### 2. Configurar .env com dados da conexão do banco de dados.
 
 ### 3. Executar migration.
+
+### Gerar chave jwt
+php artisan jwt:secret
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use App\Http\Controllers\TarefasController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::controller(TarefasController::class)->group(function () {
     Route::get('/tarefas', 'listarTarefas');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 });
 
